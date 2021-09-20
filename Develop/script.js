124c0788bd1bc27da060e53860f7aa13c6ab0185
Tyler Norman <tnorman600@gmail.com>
2021-09-20 14:59:47 -0600

    first commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
-// Assignment code here
-
+var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"
+var password = ""
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
