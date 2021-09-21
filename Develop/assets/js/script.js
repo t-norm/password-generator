2135c9f2bb2b52b57fd1d4669c58936d350e1076
Tyler Norman <tnorman600@gmail.com>
2021-09-21 10:00:57 -0600

    added functions to get a random character from applicable character sets

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,4 +1,31 @@
-// Assignment code here
+//variables
+const passLength = document.getElementById('passLength');
+const passUpper = document.getElementById('upperCase');
+const passLower = document.getElementById('lowerCase');
+const passNumbers = document.getElementById('numbers');
+const passSpecial = document.getElementById('special');
+
+// functions
+// functions to generate random characters
+function randomUpper() {
+	const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	return upperCase[Math.floor(Math.random() * upperCase.length)];
+}
+
+function randomLower() {
+	const lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+	return lowerCase[Math.floor(Math.random() * lowerCase.length)];
+}
+
+function randomNumber() {
+	const numbers = '0123456789'
+	return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function randomSymbol() {
+	const special = '!@#$%^&*(){}[]=<>/,.'
+	return special[Math.floor(Math.random() * special.length)];
+}
 
 
 // Get references to the #generate element
